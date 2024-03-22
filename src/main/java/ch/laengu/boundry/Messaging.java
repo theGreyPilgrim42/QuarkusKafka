72e7d614f2c10bcbdb056e249a61ca464a5b082c
theGreyPilgrim42 <100165947+theGreyPilgrim42@users.noreply.github.com>
2024-03-22 11:10:46 +0100

    feat: Generalize kafka message (#1)
    * refactor(entity): TextMessage to Message
    
    * feat(boundry): Improve blog validation
    
    * feat(boundry): Add comment validation

--- src/main/java/ch/laengu/boundry/Messaging.java
+++ src/main/java/ch/laengu/boundry/Messaging.java
@@ -6,27 +6,41 @@ import java.util.Arrays;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import ch.laengu.entity.TextMessage;
+import ch.laengu.entity.Message;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Messaging {
 
-    private ArrayList<String> invalidStrings = new ArrayList<>(Arrays.asList("bad", "invalid", "sucks"));
+    private ArrayList<String> invalidBlogContent = new ArrayList<>(Arrays.asList("bad", "invalid", "sucks"));
+    private ArrayList<String> invalidCommentContent = new ArrayList<>(Arrays.asList("hate", "stupid", "sucks"));
 
     @Incoming("new-blog")
-    @Outgoing("validated-text")
-    public TextMessage validateBlog(TextMessage message) {
+    @Outgoing("validated-blog")
+    public Message validateBlog(Message message) {
         Log.info("Received on topic new-blog: " + message.toString());
         String text = message.getText();
-        message.setValid(true);
-        for (String invalidString : invalidStrings) {
-            if (text.contains(invalidString)) {
-                message.setValid(false);
-            }
+        if (invalidBlogContent.contains(text)) {
+            message.setValid(false);
+        } else {
+            message.setValid(true);
         }
-        Log.info("Sending to topic validated-text: " + message.toString());
+        Log.info("Sending to topic validated-blog: " + message.toString());
+        return message;
+    }
+
+    @Incoming("new-comment")
+    @Outgoing("validated-comment")
+    public Message validateComment(Message message) {
+        Log.info("Received on topic new-comment: " + message.toString());
+        String text = message.getText();
+        if (invalidCommentContent.contains(text)) {
+            message.setValid(false);
+        } else {
+            message.setValid(true);
+        }
+        Log.info("Sending to topic validated-comment: " + message.toString());
         return message;
     }
 }
